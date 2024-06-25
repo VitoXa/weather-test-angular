fde39ee6e153cc6b0ba2d5dac5a77e8e6babb54d
Vitaliy <tyuvit17@gmail.com>
2024-06-25 20:39:04 +0400

    Add app-current-conditions to location component

--- src/app/features/weather/pages/location/location.component.ts
+++ src/app/features/weather/pages/location/location.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CurrentConditionsComponent } from '../../components/current-conditions/current-conditions.component';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [],
+  imports: [CurrentConditionsComponent],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css'
 })
 export class LocationComponent {
-  locationCode!: string | null;
+  locationKey!: string | null;
   constructor(private route: ActivatedRoute) {
 
   }
   ngOnInit(): void {
-    this.locationCode = this.route.snapshot.paramMap.get('id');
+    this.locationKey = this.route.snapshot.paramMap.get('id');
   }
 }
