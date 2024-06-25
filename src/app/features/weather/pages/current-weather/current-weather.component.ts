b4e9875c183185bddbe7b076ca16a000dd93ee4b
Vitaliy <tyuvit17@gmail.com>
2024-06-25 22:05:27 +0400

    Reflect favorites on home page

--- src/app/features/weather/pages/current-weather/current-weather.component.ts
+++ src/app/features/weather/pages/current-weather/current-weather.component.ts
@@ -3,19 +3,25 @@ import { Router } from '@angular/router';
 import { CurrentConditionsComponent } from '../../components/current-conditions/current-conditions.component';
 import { LocationSelectorComponent } from '../../components/location-selector/location-selector.component';
 import { FiveDaysForecastComponent } from '../../components/five-days-forecast/five-days-forecast.component';
+import { FavoriteLocationService } from '../../services/favorite-location.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-current-weather',
   standalone: true,
-  imports: [CurrentConditionsComponent, LocationSelectorComponent, FiveDaysForecastComponent],
+  imports: [NgFor, CurrentConditionsComponent, LocationSelectorComponent, FiveDaysForecastComponent, AsyncPipe],
   templateUrl: './current-weather.component.html',
   styleUrl: './current-weather.component.css'
 })
 export class CurrentWeatherComponent {
 
   homeLocationKey = '350540';
+  favoriteLocations!: Observable<string[]>;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private favoriteLocationService: FavoriteLocationService) { 
+    this.favoriteLocations = favoriteLocationService.favoritesLocations$;
+  }
 
   navigateToLocation(locationKey: string) {
     this.router.navigate(['/location', { id: locationKey }]);
--- src/app/features/weather/pages/location/location.component.ts
+++ src/app/features/weather/pages/location/location.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { CurrentConditionsComponent } from '../../components/current-conditions/current-conditions.component';
+import { FiveDaysForecastComponent } from '../../components/five-days-forecast/five-days-forecast.component';
 
 @Component({
   selector: 'app-location',
   standalone: true,
-  imports: [CurrentConditionsComponent],
+  imports: [CurrentConditionsComponent, FiveDaysForecastComponent],
   templateUrl: './location.component.html',
   styleUrl: './location.component.css'
 })
